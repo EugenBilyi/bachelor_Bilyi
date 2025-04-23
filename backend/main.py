633f6233745e216496f8429e7e3883dc51611b00
Yevhen <Gekon2234@gmail.com>
2025-04-23 16:48:14 +0200

    fix main

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,8 @@
-from views import app, socketio
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from backend.views import app, socketio
 
 if __name__ == '__main__':
     socketio.run(app, host='0.0.0.0', port=5000)
